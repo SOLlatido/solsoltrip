a94ad8e39b96110e761c49c6ef27a09a84946a00
leverest96 <myjade0416@gmail.com>
2023-09-03 02:13:45 +0900

    feat: member controller 생성 및 메서드 추가
    signup 메서드 추가

---
+++ backend/src/main/java/site/solsoltrip/backend/controller/MemberController.java
@@ -0,0 +1,25 @@
+package site.solsoltrip.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import site.solsoltrip.backend.dto.MemberSignUpRequestDto;
+import site.solsoltrip.backend.service.MemberService;
+
+@RestController
+@RequestMapping("/api/member")
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<Void> signup(@RequestBody final MemberSignUpRequestDto requestDto) {
+        memberService.signup(requestDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
