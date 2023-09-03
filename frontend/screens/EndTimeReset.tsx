44f3b7374e450e103eeaa183f388d353ad9f00b4
신산하 <tksgk2598@naver.com>
2023-09-04 01:16:56 +0900

    feat: EndTimeReset-page 안내문구 추가

--- frontend/screens/EndTimeReset.tsx
+++ frontend/screens/EndTimeReset.tsx
@@ -11,10 +11,16 @@ function EndTimeReset(){
     return(
         <View style={tw `flex-1 bg-white`}>
 
-            <View style={tw `flex-1 flex-row justify-center items-center`}>
-                <Ionicons style={tw `flex-1 text-4xl pt-10 pl-3`} name="close-outline" size={24} color="black" />
-                <Text style={tw `flex-2 pt-10 text-xl font-black`}>종료날짜 선택</Text>
+            <View style={tw `flex-1 justify-center items-center`}>
+                <View style={tw `flex-1 flex-row justify-center items-center mt-20`}>
+                    <Ionicons style={tw `flex-1 text-4xl pl-3`} name="close-outline" size={24} color="black" />
+                    <Text style={tw `flex-2 text-xl font-black`}>종료날짜 선택</Text>
+                </View>
+                <View style={tw `flex-1 justify-center items-center mt-2`}>
+                    <Text style={tw `text-sm text-neutral-500`}>종료 날짜는 오늘 날짜부터 선택하실 수 있어요</Text>
+                </View>
             </View>
+
 
 
             <View style={tw `flex-3 bg-white`}>
