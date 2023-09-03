e4e1baf455f92bf64265845a18ae23d74c6886bc
신산하 <tksgk2598@naver.com>
2023-09-04 00:57:22 +0900

    refactor: EndTimeReset-page Tailwind CSS 도입

--- frontend/screens/EndTimeReset.tsx
+++ frontend/screens/EndTimeReset.tsx
@@ -1,4 +1,4 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text} from 'react-native';
 import tw from 'twrnc'; 
 
 // 컴포넌트
@@ -8,14 +8,14 @@ import LongButton from '../components/ButtonItems/LongButton';
 function EndTimeReset(){
 
     return(
-        <View style={styles.containerView}>
+        <View style={tw `flex-1`}>
 
-            <View style={styles.introView}>
-                <Text style={styles.statementText}>종료날짜를 재설정해주세요</Text>
+            <View style={tw `flex-1 justify-center items-center`}>
+                <Text style={tw `pt-10 text-xl font-black`}>종료날짜를 재설정해주세요</Text>
             </View>
 
 
-            <View style={styles.calendarView}>
+            <View style={tw `flex-3 bg-white`}>
                 <Calendar/>
             </View>
 
@@ -24,54 +24,10 @@ function EndTimeReset(){
                 <LongButton content='완료'/>
             </View>
 
-            <View style={styles.bottomNavView}></View>
-
         </View>
     )
 
 }
-
-
-const styles = StyleSheet.create({
-
-    containerView: {
-        flex: 1,
-    },
-
-
-    // 상단 안내 문구 영역
-    introView: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "red",
-    },
-    
-    statementText:{
-        paddingTop:30,
-        fontSize:20,
-        fontWeight:"900",
-    },
-
-
-    // 달력 영역
-    calendarView:{
-        flex: 3,
-        backgroundColor:"yellow",
-    },
-
-    // 취소 완료 버튼 영역
-    buttonsView:{
-        flex: 1,
-        backgroundColor:"green",
-    },
-
-    // 하단 nav 영역
-    bottomNavView:{
-
-    }
-
-})
 
 
 export default EndTimeReset
