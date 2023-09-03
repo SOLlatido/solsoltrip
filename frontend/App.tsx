c9d19b28d7c5090151f76798df37826306af9d3f
Daen12 <111489407+Daen12@users.noreply.github.com>
2023-09-03 18:16:01 +0900

    feat: Tailwind 스타일링 도입
    twnrc 패키지 추가
    expo router 추가
    intro와 button 재스타일링
    쏠 캐릭터 상하 애니메이션 도입

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <>
       <Intro></Intro>
-      <StatusBar style="auto" />
+      {/* <StatusBar style="auto" /> */}
     </>
   );
 }
--- frontend/components/ButtonItems/LongButton.tsx
+++ frontend/components/ButtonItems/LongButton.tsx
@@ -1,39 +1,63 @@
-import { View, Text, StyleSheet, Pressable } from 'react-native'
-import React from 'react'
-import { Colors } from 'react-native/Libraries/NewAppScreen'
+// import { View, Text, StyleSheet, Pressable, TouchableOpacity } from 'react-native'
+// import React from 'react'
+// import { Colors } from 'react-native/Libraries/NewAppScreen'
 
-const LongButton = (props:{content:string}) => {
-  const content:string = props.content
+// const LongButton = (props:{content:string}) => {
+//   const content:string = props.content
+//   return (
+//     <View style={s.container}>
+//       <TouchableOpacity
+//         activeOpacity={0.8}
+//         style={s.button}
+//         onPress={()=>{console.warn("hello")}}
+//       >
+//       <Text style={s.text}>{content}</Text>
+//       </TouchableOpacity>
+//     </View>
+//   )
+// }
+
+// const s = StyleSheet.create({
+//   container : {
+//     flex:1,
+//     padding:20,
+//     width : "100%"
+//   },
+//   button : {
+//     backgroundColor : "#7B5AF3",
+//     height : 50,
+//     borderRadius : 15,
+//     justifyContent : "center",
+//     alignItems : "center",
+//   },
+//   text : {
+//     fontSize : 20,
+//     fontWeight : '500',
+//     color : "#fff"
+//   }
+// })
+
+// export default LongButton
+
+import { View, Text, TouchableOpacity } from 'react-native';
+import React from 'react';
+import tw from 'twrnc'; // Import the tw function
+
+const LongButton = (props: { content: string }) => {
+  const content: string = props.content;
   return (
-    <View style={s.container}>
-      <Pressable
-        style={s.button}
-        onPress={()=>{console.warn("hello")}}
+    <View style={tw `flex-1 p-4 w-full items-center`}>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        style={tw`w-6/7 bg-[#7B5AF3] h-12 rounded-[4] justify-center items-center `}
+        onPress={() => {
+          console.warn(`hello`);
+        }}
       >
-      <Text style={s.text}>{content}</Text>
-      </Pressable>
+        <Text style={tw `text-white text-lg font-semibold`}>{content}</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-const s = StyleSheet.create({
-  container : {
-    flex:1,
-    padding:20,
-    width : "100%"
-  },
-  button : {
-    backgroundColor : "#7B5AF3",
-    height : 50,
-    borderRadius : 15,
-    justifyContent : "center",
-    alignItems : "center",
-  },
-  text : {
-    fontSize : 20,
-    fontWeight : '500',
-    color : "#fff"
-  }
-})
-
-export default LongButton
+export default LongButton;
