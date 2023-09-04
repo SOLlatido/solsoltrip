a572a3bb29e3312955eb63dfbe709be444cb8126
Daen12 <111489407+Daen12@users.noreply.github.com>
2023-09-04 23:28:39 +0900

    feat: 로그인 회원가입 레이아웃 정돈

--- frontend/screens/Login.tsx
+++ frontend/screens/Login.tsx
@@ -28,15 +28,21 @@ const Login:React.FC<NavigationProps> = ({navigation}) => {
       <View style={tw `flex-2 w-full h-full`}>
         <View style={tw `flex-1 w-full items-center`}>
           <View style={tw `flex-1`}></View>
-          <Text style={tw `flex-1 text-2xl mt-5 text-[#222]`}>아이디 / 비밀번호</Text>
+          <View style={tw `flex-1 items-center`}>
+          <Text style={tw `text-2xl font-bold mt-5 text-[#222]`}>로그인</Text>
+          <Text style={tw `mt-2`}>아이디 / 비밀번호를 입력해주세요</Text>
+          
+          </View>
 
-          <View style={tw `flex-2 items-center`}>
+          <View style={tw `flex-2`}>
+          <Text style={tw `pl-2 mb-2 text-[#555]`}>아이디</Text>
           <TextInput
               style={tw`bg-white rounded-lg w-80 h-12 px-4 mb-4`}
               placeholder="아이디 입력"
               onChangeText={(text) => setId(text)}
               value={id}
             />
+            <Text style={tw `pl-2 mb-2 text-[#555]`}>비밀번호</Text>
             <TextInput
               style={tw`bg-white rounded-lg w-80 h-12 px-4`}
               placeholder="영문자, 숫자, 특수문자 혼용(8~15자)"
@@ -44,13 +50,13 @@ const Login:React.FC<NavigationProps> = ({navigation}) => {
               onChangeText={(text) => setPassword(text)}
               value={password}
             />
-            <Text style={tw `mt-4 text-[#444]`}>처음이신가요? <Text onPress={handleSignUp} style={tw `mt-4 text-[#0046FF]`}>회원가입</Text></Text>
+            <Text style={tw `mt-4 text-[#444] self-center`}>처음이신가요? <Text onPress={handleSignUp} style={tw `mt-4 text-[#0046FF]`}>회원가입</Text></Text>
             
             </View>
         </View>
       </View>
 
-      <LongButton content='메인으로' onPress={handleMain}></LongButton>
+      {/* <LongButton content='메인으로' onPress={handleMain}></LongButton> */}
       <LongButton content='로그인' onPress={handleLogin}></LongButton>
   
     </View>
--- frontend/screens/SignUp.tsx
+++ frontend/screens/SignUp.tsx
@@ -23,15 +23,22 @@ const SignUp:React.FC<NavigationProps> = ({navigation}) => {
       <View style={tw `flex-2 w-full h-full`}>
         <View style={tw `flex-1 w-full items-center`}>
           <View style={tw `flex-1`}></View>
-          <Text style={tw `flex-1 text-2xl mt-5 text-[#222]`}>회원가입</Text>
 
-          <View style={tw `flex-2 items-center`}>
-          <TextInput
-              style={tw`bg-white rounded-lg w-80 h-12 px-4 mb-4`}
-              placeholder="이름을 입력해주세요"
-              onChangeText={(text) => setId(text)}
-              value={id}
-            />
+          <View style={tw `flex-1 items-center`}>
+          <Text style={tw `text-2xl font-bold mt-5 text-[#222]`}>회원가입</Text>
+          <Text style={tw `mt-2`}>회원 등록을 위한 정보를 입력해주세요.</Text>
+          </View>
+
+          <View style={tw `flex-2`}>
+            <Text style={tw `pl-2 mb-2 text-[#555]`}>이름</Text>
+            <TextInput
+            
+                style={tw`bg-white rounded-lg w-80 h-12 px-4 mb-4`}
+                placeholder="이름을 입력해주세요"
+                onChangeText={(text) => setId(text)}
+                value={id}
+              />
+            <Text style={tw `pl-2 mb-2 text-[#555]`}>아이디</Text>
             <TextInput
               style={tw`bg-white rounded-lg w-80 h-12 px-4 mb-4`}
               placeholder="아이디를 입력해주세요"
@@ -39,6 +46,7 @@ const SignUp:React.FC<NavigationProps> = ({navigation}) => {
               onChangeText={(text) => setPassword(text)}
               value={password}
             />
+            <Text style={tw `pl-2 mb-2 text-[#555]`}>비밀번호</Text>
             <TextInput
               style={tw`bg-white rounded-lg w-80 h-12 px-4 mb-4`}
               placeholder="비밀번호를 입력해주세요"
@@ -46,13 +54,14 @@ const SignUp:React.FC<NavigationProps> = ({navigation}) => {
               onChangeText={(text) => setPassword(text)}
               value={password}
             />
+            {/* <Text style={tw `pl-2 mb-2 text-[#555]`}>휴대폰 번호</Text>
             <TextInput
               style={tw`bg-white rounded-lg w-80 h-12 px-4`}
-              placeholder="이메일을 입력해주세요"
+              placeholder="휴대폰 번호를 입력해주세요"
               secureTextEntry={true} // This hides the input as it's a password
               onChangeText={(text) => setPassword(text)}
               value={password}
-            />
+            /> */}
             
             </View>
         </View>
