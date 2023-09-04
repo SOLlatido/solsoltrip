d2e6749243220e62e243d604c94f52b47a5a7877
신산하 <tksgk2598@naver.com>
2023-09-05 01:30:47 +0900

    docs: recoil 라이브러리 설치

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,11 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import { useRouter } from 'expo-router';
 import AppNavigation from './navigation/AppNavigation';
 
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from 'recoil';
+
 
 export default function App() {
   return (
-    <>
+    <RecoilRoot>
       <AppNavigation></AppNavigation>
-    </>
+    </RecoilRoot>
   );
 }
