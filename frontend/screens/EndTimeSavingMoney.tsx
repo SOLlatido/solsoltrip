83d7937eaeaa3a4eae55ee6812b32e5330ad355b
신산하 <tksgk2598@naver.com>
2023-09-07 01:19:35 +0900

    feat: endTime-saving-account header UI 구성

--- frontend/screens/EndTimeSavingMoney.tsx
+++ frontend/screens/EndTimeSavingMoney.tsx
@@ -1,11 +1,31 @@
-import {View, Text} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {View, Text, ImageBackground} from 'react-native';
 import tw from 'twrnc'; 
 import { Ionicons } from "@expo/vector-icons";
 
+// 이미지
+import starrynight from '../assets/images/starrynight_bg.jpg';
+
 const EndTimeSavingMoney = () => {
+    
+    const [saving, setSaving] = useState("20,000");
+
     return(
         <View style={tw`flex-1`}>
-            <Text>하이</Text>
+            <ImageBackground source={starrynight} style={tw `w-full h-full bg-cover absolute`}></ImageBackground>
+            
+            <View style={tw `flex-2 justify-center items-center`}>
+                <Text style={tw `text-xl text-white mt-10`}>동행통장 잔여 금액</Text>
+                <Text style={tw `text-5xl font-bold text-white`}>{saving}원</Text>
+            </View>
+
+            <View style={tw `flex-3 bg-orange-100`}>
+
+            </View>
+
+            <View style={tw `flex-1 bg-blue-100`}>
+
+            </View>
         </View>
     )
 }
