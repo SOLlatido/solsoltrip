40525a6fb4520b98f3401e1f00192172ed10c277
신산하 <tksgk2598@naver.com>
2023-09-07 03:29:54 +0900

    feat: endTime-saving-account 애니메이션 추가

--- frontend/screens/EndTimeSavingMoney.tsx
+++ frontend/screens/EndTimeSavingMoney.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import {View, Text, ImageBackground} from 'react-native';
 import tw from 'twrnc'; 
 import { StackNavigationProp } from '@react-navigation/stack';
+import * as Animatable from 'react-native-animatable'; // 애니메이션 라이브러리 추가
 
 // 이미지
 import starrynight from '../assets/images/starrynight_bg.jpg';
@@ -26,18 +27,29 @@ const EndTimeSavingMoney:React.FC<EndTimeSavingMoneyProps> = ({navigation}) => {
     //     navigation.navigate("EndTimeSavingMoney");
     // }
 
+    const [animation1, setAnimation1] = useState(null);
+    const [animation2, setAnimation2] = useState(null);
+
+    useEffect(() => {
+        if (animation1) animation1.slideInUp(1000); // 첫 번째 애니메이션
+      }, [animation1]);
+    
+      useEffect(() => {
+        if (animation2) animation2.slideInUp(2000); // 두 번째 애니메이션
+      }, [animation2]);
+
     return(
         <View style={tw`flex-1`}>
-            <ImageBackground source={starrynight} style={tw `w-full h-full bg-cover absolute`}></ImageBackground>
+            <ImageBackground source={starrynight} style={tw `w-full h-full bg-cover absolute`}/>
             
-            <View style={tw `flex-2 justify-center items-center`}>
+            <Animatable.View ref={(ref) => setAnimation1(ref)} style={tw `flex-2 justify-center items-center`}>
                 <Text style={tw `text-xl text-white mt-10`}>동행통장 잔여 금액</Text>
                 <Text style={tw `text-5xl font-bold text-white`}>{saving}원</Text>
-            </View>
+            </Animatable.View>
 
-            <View style={tw `flex-4 items-center justify-center`}>
+            <Animatable.View ref={(ref) => setAnimation2(ref)} style={tw `flex-4 items-center justify-center`}>
                 <SavingMoneySlider/>
-            </View>
+            </Animatable.View>
 
             <View style={tw `flex-1 flex-row`}>
                 <LongButton content='이전' onPress={handleSavingMoney}/>
