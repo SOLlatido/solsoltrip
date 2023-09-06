925c00e42374166dee3670bfa840a9de25e7a022
leverest96 <myjade0416@gmail.com>
2023-09-07 01:28:45 +0900

    feat: 이벤트 controller 메서드 추가
    주변 이벤트 탐색을 위한 nearbyInform 메서드 추가

--- backend/src/main/java/site/solsoltrip/backend/controller/EventController.java
+++ backend/src/main/java/site/solsoltrip/backend/controller/EventController.java
@@ -4,11 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import site.solsoltrip.backend.dto.EventRequestDto;
+import site.solsoltrip.backend.dto.EventResponseDto;
 import site.solsoltrip.backend.service.EventService;
 
 @RestController
@@ -24,4 +22,10 @@ public class EventController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @GetMapping("/nearby")
+    public ResponseEntity<EventResponseDto.nearbyInform> nearbyInform(@RequestBody @Validated final EventRequestDto.nearbyInform requestDto) {
+        final EventResponseDto.nearbyInform responseDto = eventService.nearbyInform(requestDto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+    }
 }
