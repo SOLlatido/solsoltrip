5545e109e7b9f2c62e1b668424f3cf4c41de070c
leverest96 <myjade0416@gmail.com>
2023-09-07 01:20:32 +0900

    refactor: 멤버 repository 수정
    findByMemberSeq 추가

--- backend/src/main/java/site/solsoltrip/backend/repository/MemberRepository.java
+++ backend/src/main/java/site/solsoltrip/backend/repository/MemberRepository.java
@@ -6,5 +6,7 @@ import site.solsoltrip.backend.entity.Member;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByMemberSeq(final Long memberSeq);
+
     Optional<Member> findById(final String id);
 }
