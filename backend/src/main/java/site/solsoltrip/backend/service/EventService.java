5db903f2b8344abdc6a73409096376f82e385e90
leverest96 <myjade0416@gmail.com>
2023-09-07 01:28:24 +0900

    feat: 이벤트 service 메서드 추가
    주변 이벤트 탐색을 위한 nearbyInform 메서드 추가

--- backend/src/main/java/site/solsoltrip/backend/service/EventService.java
+++ backend/src/main/java/site/solsoltrip/backend/service/EventService.java
@@ -8,9 +8,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 import site.solsoltrip.backend.dto.EventRequestDto;
+import site.solsoltrip.backend.dto.EventResponseDto;
 import site.solsoltrip.backend.entity.Event;
+import site.solsoltrip.backend.entity.Member;
+import site.solsoltrip.backend.entity.MemberEvent;
 import site.solsoltrip.backend.repository.EventRepository;
+import site.solsoltrip.backend.repository.MemberEventRepository;
+import site.solsoltrip.backend.repository.MemberRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,6 +24,8 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class EventService {
     private final EventRepository eventRepository;
+    private final MemberRepository memberRepository;
+    private final MemberEventRepository memberEventRepository;
 
     private WebClient webClient;
 
@@ -44,6 +52,95 @@ public class EventService {
                 .build();
 
         eventRepository.save(event);
+    }
+
+    public EventResponseDto.nearbyInform nearbyInform(final EventRequestDto.nearbyInform requestDto) {
+        final Member member = memberRepository.findByMemberSeq(requestDto.memberSeq()).orElseThrow(
+                () -> new IllegalArgumentException("존재하지 않는 유저입니다.")
+        );
+
+        final List<Event> eventList = eventRepository.findByRegion(requestDto.region());
+
+        if (eventList.isEmpty()) {
+            return null;
+        }
+
+        List<EventResponseDto.EventResponseVO> responseVOList = new ArrayList<>();
+
+        for (Event event : eventList) {
+            final MemberEvent memberEvent = memberEventRepository
+                    .findByMemberSeqAndEventSeqJoinFetchMemberAndEvent(member.getMemberSeq(), event.getEventSeq())
+                    .orElseGet(() -> memberEventRepository.save(MemberEvent.builder()
+                                    .event(event)
+                                    .member(member)
+                                    .isDone(false)
+                            .build())
+            );
+
+            if (memberEvent.getIsDone()) {
+                continue;
+            }
+
+            double dist = coordinateToMeter(event.getX(), event.getY(), requestDto.x(), requestDto.y());
+
+            if (dist > 500) {
+                continue;
+            }
+
+            short plane = 0;
+            double ratio = 0;
+
+            if (event.getY() - requestDto.y() > 0 && event.getX() - requestDto.x() > 0) {
+                plane = 1;
+            } else if (event.getY() - requestDto.y() > 0 && event.getX() - requestDto.x() < 0) {
+                plane = 2;
+            } else if (event.getY() - requestDto.y() < 0 && event.getX() - requestDto.x() < 0) {
+                plane = 3;
+            } else if (event.getY() - requestDto.y() < 0 && event.getX() - requestDto.x() > 0) {
+                plane = 4;
+            } else if (event.getY() - requestDto.y() == 0) {
+                ratio = 1;
+            } else {
+                ratio = 0;
+            }
+
+            if (event.getX() - requestDto.x() != 0 || event.getY() - requestDto.y() != 0) {
+                ratio = Math.abs(event.getX() - requestDto.x()) / Math.abs(event.getY() - requestDto.y());
+            }
+
+            EventResponseDto.EventResponseVO responseVO = EventResponseDto.EventResponseVO.builder()
+                    .name(event.getName())
+                    .plane(plane)
+                    .ratio(ratio)
+                    .build();
+
+            responseVOList.add(responseVO);
+        }
+
+        return EventResponseDto.nearbyInform.builder()
+                .responseVOList(responseVOList)
+                .build();
+    }
+
+    private static double coordinateToMeter(double eventX, double eventY, double curX, double curY) {
+        double theta = eventY - curY;
+
+        double dist = Math.sin(deg2rad(eventX)) * Math.sin(deg2rad(curX)) +
+                Math.cos(deg2rad(eventX)) * Math.cos(deg2rad(curX)) * Math.cos(deg2rad(theta));
+
+        dist = Math.acos(dist);
+
+        dist = rad2deg(dist);
+
+        return dist * 60 * 1.1515 * 1609.344;
+    }
+
+    private static double deg2rad(double deg){
+        return (deg * Math.PI/180.0);
+    }
+    //radian(라디안)을 10진수로 변환
+    private static double rad2deg(double rad){
+        return (rad * 180 / Math.PI);
     }
 
     private String sendRequest(final double latitude, final double longitude) {
