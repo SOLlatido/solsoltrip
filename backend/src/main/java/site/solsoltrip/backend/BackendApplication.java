f459325952e7b6611fe7f9729d558949bc207fff
leverest96 <myjade0416@gmail.com>
2023-09-08 20:05:48 +0900

    refactor: application.java 파일의 불필요한 import 제거

--- backend/src/main/java/site/solsoltrip/backend/BackendApplication.java
+++ backend/src/main/java/site/solsoltrip/backend/BackendApplication.java
@@ -3,7 +3,6 @@ package site.solsoltrip.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class BackendApplication {
