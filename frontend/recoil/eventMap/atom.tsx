38faf4aaa76ac24587df0cc600c42fb5d7195c98
신산하 <tksgk2598@naver.com>
2023-09-09 03:33:39 +0900

    feat: EventMap 500m 내로 관광지가 있을 때 알림보내기 기능 구현중
    0번 배열의 marker만 display:false로 바뀌고, 1번 배열의 marker는 바뀌지 않는 문제가 있다.
    
    prevLatLng 변수가 초기값 0에서 현재 위치 값으로 안바뀌는 이슈가 있다.

---
+++ frontend/recoil/eventMap/atom.tsx
@@ -0,0 +1,50 @@
+import { atom } from 'recoil';
+import {AnimatedRegion} from 'react-native-maps';
+
+
+const initialState = {
+    mode: "wait",
+    latitude: 0,
+    longitude: 0,
+    distanceTravelled: 0, // 이동한 거리 
+    prevLatLng: {},
+    // coordinate: new AnimatedRegion({
+    //     latitude: 0,
+    //     longitude: 0,
+    //     latitudeDelta: 0,
+    //     longitudeDelta: 0,
+    // }),
+    characterLocations: [{ // 캐릭터 위치 리스트
+        latitude: 36.3688253,
+        longitude: 127.3468684,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+        title: "충남대학교",
+        description: "대전의 큰 학교이자 유명한 관광지",
+        display: true,
+    },{
+        latitude: 36.3557439,
+        longitude: 127.3471908,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+        title: "계룡스파텔",
+        description: "스파",
+        display: true,
+    }],
+}
+
+export const eventMapState = atom<eventMapType>({
+    key:'eventMapState',
+    default:initialState,
+});
+
+
+export interface eventMapType {
+    mode: string,
+    latitude: number,
+    longitude: number,
+    distanceTravelled: number,
+    prevLatLng: object,
+    // coordinate: any[],
+    characterLocations: any[],
+}
--- frontend/screens/EventMap.tsx
+++ frontend/screens/EventMap.tsx
@@ -1,85 +1,214 @@
-import React, { useState, useEffect,useLayoutEffect } from 'react';
-import { Platform, Text, View, StyleSheet,Dimensions,Image } from 'react-native';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
+import { Platform, Text, View, StyleSheet, Dimensions, Image } from 'react-native';
 import * as Location from 'expo-location';
-import MapView,{Marker} from 'react-native-maps';
+import MapView, { Marker, AnimatedRegion, Animated, Polyline } from 'react-native-maps';
 import tw from 'twrnc';
+import haversine from 'haversine';
+
+// recoil
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { eventMapState } from '../recoil/eventMap/atom';
 
 // 캐릭터 이미지
 import sol_charater1 from '../assets/character/sol_character1.png';
 
 const EventMap = () => {
-    const [location, setLocation] = useState<any>(null);
-    const [errorMsg, setErrorMsg] = useState<string|null>(null);
+  const [location, setLocation] = useState<any>(null);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const [distanceTravelled, setDistanceTravelled] = useState(0);
+  const [prevLatLng, setPrevLatLng] = useState({ latitude: 0, longitude: 0 });
+  const [routeCoordinates, setRouteCoordinates] = useState([]); //유저가 움직인 이동경로
 
-    // 내 위치를 찾는 함수
-    useLayoutEffect(() => {
-        (async () => {
-          let { status } = await Location.requestForegroundPermissionsAsync();
-          if (status !== 'granted') {
-            setErrorMsg('Permission to access location was denied');
-            return;
-          }
-    
-          let location = await Location.getCurrentPositionAsync({});
-    
-          setLocation({latitude:location.coords.latitude,longitude:location.coords.longitude,
+  const [eventMap, setEventMap] = useRecoilState(eventMapState);
+  const { characterLocations } = eventMap;
+
+  // 거리를 계산하는 함수
+  const calcDistance = (newLatLng: any) => {
+    console.log(`prevLatLng = ${prevLatLng.longitude}`);
+    console.log(`newLatLng = ${newLatLng.longitude}`);
+    return haversine(prevLatLng, newLatLng) || 0;
+  };
+
+  // 유저의 위치와 마커 간의 거리를 확인하는 함수 500m 알람범위
+  const isWithin500m = (userLocation: any, markerLocation: any) => {
+    const distance = calcDistance(userLocation);
+    console.log(`distance = ${distance}`);
+    return distance <= 0.5; // 500m = 0.5 km
+  };
+
+  // 내 위치를 찾는 함수
+  useLayoutEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+
+      setLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    })();
+  }, []);
+
+  // 이동 위치 추적
+  useEffect(() => {
+    const watchLocation = async () => {
+      const locationOptions = {
+        accuracy: Location.Accuracy.Balanced,
+        timeInterval: 5000,
+        distanceInterval: 10,
+      };
+      const locationSubscription = await Location.watchPositionAsync(
+        locationOptions,
+        (position) => {
+          const { latitude, longitude } = position.coords;
+
+          const newCoordinate = {
+            latitude,
+            longitude,
+          };
+
+          setLocation({
+            latitude: latitude,
+            longitude: longitude,
             latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,})
-        console.log(location);
-        })();
+            longitudeDelta: 0.0421,
+          });
+
+          setRouteCoordinates((prevCoordinates) => [...prevCoordinates, newCoordinate]);
+
+          // distanceTravelled를 업데이트하지 않고, 이동 거리만 계산하여 사용
+          const newDistance = calcDistance(newCoordinate);
+          setDistanceTravelled((prevDistance) => prevDistance + newDistance);
+
+          console.log(newCoordinate.latitude);
+          setPrevLatLng(newCoordinate);
+
+          // 500m 안에 들어왔는지 확인 -> alert
+          let markerLocation = null;
+          let isWithin500mFlag = false;
+
+          for (let i = 0; i < characterLocations.length; i++) {
+            markerLocation = characterLocations[i];
+
+            // 이미 본 캐릭터는 처리하지 않음
+            if (!markerLocation.display) continue;
+
+            isWithin500mFlag = isWithin500m(newCoordinate, markerLocation);
     
-      }, []);
+            if (isWithin500mFlag && markerLocation.display) {
 
+                console.log(`Entering if block with i = ${i}`);
 
-    return(
-        <View>
-            <MapView
-                initialRegion={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                }}
-                style={styles.map}
-                showsUserLocation
-                followsUserLocation
-                loadingEnabled
-                region={location}>
+                // Recoil 상태를 업데이트하여 display 속성 변경
+                setEventMap((prevEventMapState) => {
 
-                    <Marker
-                        coordinate={{
-                            latitude: 36.3688253,
-                            longitude: 127.3468684,
-                            // latitudeDelta: 0.0922,
-                            // longitudeDelta: 0.0421,
-                        }}
-                        title={'충남대학교'}
-                        description={'대전의 큰 학교이자 유명한 관광지'}
-                    >
-                        <Image
-                            source={sol_charater1} // 이미지를 직접 지정합니다.
-                            style={{ width: 40, height: 40 }} // 이미지 크기를 조정하세요.
-                        />
-                    </Marker>
-        
-            </MapView>
-        
-        </View>
-    )
-}
+                    const updatedCharacterLocations = prevEventMapState.characterLocations.map(
+                      (characterLocation, index) => {
+                        if (index === i) {
+                          // 현재 반복 중인 요소가 변경 대상이라면 display를 false로 변경
+                          return { ...characterLocation, display: false };
+                        }
+                        // 변경 대상이 아니면 그대로 반환
+                        return characterLocation;
+                      }
+                    );
+                  
+                    // 업데이트된 characterLocations를 포함한 새로운 상태를 반환
+                    return {
+                      ...prevEventMapState,
+                      characterLocations: updatedCharacterLocations,
+                    };
+                });
+                  
+                  
+                  
+                  
+                  
+                console.log(markerLocation);
 
-export default EventMap
+                
 
+              // 유저가 500m 이내에 마커에 접근했을 때 알림 표시 / 한개라도 보이면 표시
+              if(markerLocation.title!=null)
+                alert(`500m 이내에 ${markerLocation.title} 관광지가 보입니다!`);
+              break;
+            }
+          }
+        }
+      );
+
+      return () => {
+        // 컴포넌트가 언마운트될 때 위치 감지 구독 해제
+        locationSubscription.remove();
+      };
+    };
+
+    watchLocation();
+  }, []);
+
+  useEffect(() => {
+    console.log("Updated eventMap:", eventMap);
+  }, [eventMap, prevLatLng]);
+
+  return (
+    <View>
+      <MapView
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        style={styles.map}
+        showsUserLocation
+        followsUserLocation
+        loadingEnabled={true}
+        region={location}
+      >
+        <Polyline coordinates={routeCoordinates} strokeWidth={3} />
+
+        {characterLocations.map((place, index) => {
+          return (
+            <Marker
+              coordinate={{
+                latitude: place?.latitude,
+                longitude: place?.longitude,
+                latitudeDelta: place?.latitudeDelta,
+                longitudeDelta: place?.longitudeDelta,
+              }}
+              title={place?.title}
+              description={place?.description}
+            >
+              <Image
+                source={sol_charater1} // 이미지를 직접 지정합니다.
+                style={{ width: 40, height: 40 }} // 이미지 크기를 조정하세요.
+              />
+            </Marker>
+          );
+        })}
+      </MapView>
+    </View>
+  );
+};
+
+export default EventMap;
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    map: {
-      width: Dimensions.get('window').width,
-      height: Dimensions.get('window').height,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
 });
