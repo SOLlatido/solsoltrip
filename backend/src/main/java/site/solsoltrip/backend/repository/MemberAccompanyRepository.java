a76fa9619e8104387990e57e5a58601b8306f3ad
KimMinSik <rlaalstlr0108@gmail.com>
2023-09-09 17:25:01 +0900

    feat: 멤버 동행통장 repository 추가

---
+++ backend/src/main/java/site/solsoltrip/backend/repository/MemberAccompanyRepository.java
@@ -0,0 +1,7 @@
+package site.solsoltrip.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import site.solsoltrip.backend.entity.MemberAccompany;
+
+public interface MemberAccompanyRepository extends JpaRepository<MemberAccompany, Long> {
+}
