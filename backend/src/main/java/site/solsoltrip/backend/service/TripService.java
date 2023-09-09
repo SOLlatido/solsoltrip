873fb780b332a8feef683855d402b7fa87deba65
KimMinSik <rlaalstlr0108@gmail.com>
2023-09-09 15:51:05 +0900

    refactor: TripService 신한은행 API 데이터 저장 로직 수정

--- backend/src/main/java/site/solsoltrip/backend/service/TripService.java
+++ backend/src/main/java/site/solsoltrip/backend/service/TripService.java
@@ -1,18 +1,29 @@
 package site.solsoltrip.backend.service;
 
-import com.google.gson.Gson;
+import com.google.gson.*;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
+import site.solsoltrip.backend.dto.ShbhackRequestDto;
+import site.solsoltrip.backend.dto.ShbhackResponseDto;
 import site.solsoltrip.backend.dto.TripRequestDto;
+import site.solsoltrip.backend.entity.RegistedAccount;
+import site.solsoltrip.backend.repository.MemberRepository;
+import site.solsoltrip.backend.repository.RegistedAccountRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class TripService {
+    private final RegistedAccountRepository registedAccountRepository;
+    private final MemberRepository memberRepository;
+
     private WebClient webClient;
 
     @PostConstruct
@@ -22,11 +33,25 @@ public class TripService {
 
     @Transactional
     public void validation(final TripRequestDto.validation requestDto) {
-        final String bankListObject = sendRequest(requestDto.memberSeq(), "/v1/account");
+        if (registedAccountRepository.findByMemberSeqJoinFetchMember(requestDto.memberSeq()).isEmpty()) {
+            final String bankListObject = sendRequest(requestDto.memberSeq(), "/v1/account");
 
-        final Gson gson = new Gson();
+            final ShbhackResponseDto responseDto = stringToShbhackResponseDto(bankListObject);
 
-        final ShbhackResponseDto responseDto = gson.fromJson(bankListObject, ShbhackResponseDto.class);
+            for (ShbhackResponseDto.DataBody.DepositAndSavings data : responseDto.getDataBody().get조회내역1()) {
+                RegistedAccount account = RegistedAccount.builder()
+                        .type(data.get구분())
+                        .account(data.get계좌번호())
+                        .name(data.get상품명())
+                        .balance(Integer.parseInt(data.get잔액()))
+                        .member(memberRepository.findByMemberSeq(requestDto.memberSeq()).get())
+                        .build();
+
+                registedAccountRepository.save(account);
+            }
+        }
+
+        // Todo : 동행통장으로 계좌를 사용할 수 있는지 여부에 대한 로직
     }
 
     @Transactional
@@ -45,5 +70,45 @@ public class TripService {
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
+    }
+
+    @Transactional
+    public ShbhackResponseDto stringToShbhackResponseDto(String jsonObject) {
+        JsonElement element = JsonParser.parseString(jsonObject);
+
+        JsonObject object = element.getAsJsonObject();
+
+        JsonObject dataHeader = object.get("dataHeader").getAsJsonObject();
+        JsonObject dataBody = object.get("dataBody").getAsJsonObject();
+
+        JsonArray checkHistory = dataBody.get("조회내역1").getAsJsonArray();
+
+        List<ShbhackResponseDto.DataBody.DepositAndSavings> 조회내역 = new ArrayList<>();
+
+        int bankDataNum = Integer.parseInt(dataBody.get("반복횟수1").getAsString());
+
+        for (int i = 0; i < bankDataNum; i++) {
+            조회내역.add(ShbhackResponseDto.DataBody.DepositAndSavings.builder()
+                    .구분(checkHistory.get(i).getAsJsonObject().get("구분").getAsString())
+                    .계좌번호(checkHistory.get(i).getAsJsonObject().get("계좌번호").getAsString())
+                    .상품명(checkHistory.get(i).getAsJsonObject().get("상품명").getAsString())
+                    .잔액(checkHistory.get(i).getAsJsonObject().get("잔액(원화)").getAsString())
+                    .build());
+        }
+
+        ShbhackResponseDto responseDto = ShbhackResponseDto.builder()
+                .dataHeader(ShbhackResponseDto.DataHeader.builder()
+                        .successCode(dataHeader.get("successCode").getAsString())
+                        .resultCode(dataHeader.get("resultCode").getAsString())
+                        .resultMessage(dataHeader.get("resultMessage").getAsString())
+                        .build())
+                .dataBody(ShbhackResponseDto.DataBody.builder()
+                        .고객명(dataBody.get("고객명").getAsString())
+                        .반복횟수(dataBody.get("반복횟수1").getAsString())
+                        .조회내역1(조회내역)
+                        .build())
+                .build();
+
+        return responseDto;
     }
 }
