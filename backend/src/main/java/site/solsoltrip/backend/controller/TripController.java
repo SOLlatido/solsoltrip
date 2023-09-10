295513e3f300253a7afb2b21d259ada557523148
KimMinSik <rlaalstlr0108@gmail.com>
2023-09-10 21:07:39 +0900

    feat: 여행 controller 메서드 추가 여행 상세페이지로 이동하는 movetoTripDetail 메서드 추가

--- backend/src/main/java/site/solsoltrip/backend/controller/TripController.java
+++ backend/src/main/java/site/solsoltrip/backend/controller/TripController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import site.solsoltrip.backend.dto.TripRequestDto;
 import site.solsoltrip.backend.service.TripService;
 
@@ -29,5 +26,12 @@ public class TripController {
         tripService.registAccount(requestDto);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/detail")
+    public ResponseEntity<Void> movetoTripDetail(@RequestBody @Validated final TripRequestDto.tripDetail requestDto) {
+        TripResponseDto.tripDetail responseDto = tripService.movetoTripDetail(requestDto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 }
