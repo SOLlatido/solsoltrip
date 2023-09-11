0f2740e890f9735d4247bda50114a263596ec32a
leverest96 <myjade0416@gmail.com>
2023-09-11 12:48:46 +0900

    feat: 멤버 controller에 카카오 로그인 관련 로직 추가

--- backend/src/main/java/site/solsoltrip/backend/controller/MemberController.java
+++ backend/src/main/java/site/solsoltrip/backend/controller/MemberController.java
@@ -1,16 +1,16 @@
 package site.solsoltrip.backend.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import site.solsoltrip.backend.dto.KakaoUserInfo;
 import site.solsoltrip.backend.dto.MemberRequestDto;
 import site.solsoltrip.backend.dto.MemberResponseDto;
 import site.solsoltrip.backend.service.MemberService;
+import site.solsoltrip.backend.utility.CookieUtility;
 
 @RestController
 @RequestMapping("/api/member")
@@ -25,9 +25,16 @@ public class MemberController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @PostMapping("/login")
+    @GetMapping("/login")
     public ResponseEntity<MemberResponseDto.login> login(@RequestBody @Validated final MemberRequestDto.login requestDto) {
         MemberResponseDto.login responseDto = memberService.login(requestDto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+    }
+
+    @GetMapping("/kakao")
+    public ResponseEntity<MemberResponseDto.KakaoTokenInfo> kakaoSync(@RequestParam final String code, @RequestParam final Long state, HttpServletResponse response) {
+        MemberResponseDto.KakaoTokenInfo responseDto = memberService.kakaoSync(code, state);
 
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
