d5adba6f14e1525e9a22a8bf6140cb28111fa973
leverest96 <myjade0416@gmail.com>
2023-09-11 12:48:25 +0900

    feat: 멤버 service에 카카오 로그인 관련 로직 추가

--- backend/src/main/java/site/solsoltrip/backend/service/MemberService.java
+++ backend/src/main/java/site/solsoltrip/backend/service/MemberService.java
@@ -8,13 +8,18 @@ import site.solsoltrip.backend.dto.MemberRequestDto;
 import site.solsoltrip.backend.dto.MemberResponseDto;
 import site.solsoltrip.backend.entity.Member;
 import site.solsoltrip.backend.entity.Role;
+import site.solsoltrip.backend.oauth.KakaoOAuth2;
 import site.solsoltrip.backend.repository.MemberRepository;
+
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+
+    private final KakaoOAuth2 kakaoOAuth2;
 
     private final PasswordEncoder passwordEncoder;
 
@@ -49,8 +54,49 @@ public class MemberService {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
 
-        return MemberResponseDto.login.builder()
-                .name(member.getName())
+        String accessToken = "access-token";
+
+        MemberResponseDto.KakaoTokenInfo kakaoTokenInfo = null;
+
+        if (member.getKakaoEmail() != null) {
+            kakaoTokenInfo = kakaoOAuth2.refreshKakaoToken(member.getKakaoRefreshToken());
+
+            if (!kakaoTokenInfo.kakaoRefreshToken().equals("null")) {
+                member.updateKakaoRefreshToken(kakaoTokenInfo.kakaoRefreshToken());
+            }
+        }
+
+        return kakaoTokenInfo == null ?
+                MemberResponseDto.login.builder()
+                        .name(member.getName())
+                        .accessToken(accessToken)
+                        .build() :
+                MemberResponseDto.login.builder()
+                        .name(member.getName())
+                        .accessToken(accessToken)
+                        .kakaoAccessToken(kakaoTokenInfo.kakaoAccessToken())
+                        .build();
+    }
+
+    @Transactional
+    public MemberResponseDto.KakaoTokenInfo kakaoSync(final String code, final Long state) {
+        final MemberResponseDto.KakaoInfo userInfo = kakaoOAuth2.kakaoSync(code);
+
+        final String email = userInfo.userInfo().getKakao_account().getEmail();
+
+        final Member member = memberRepository.findByMemberSeq(state).orElseThrow(
+                () -> new IllegalArgumentException("존재하지 않는 유저입니다.")
+        );
+
+        final Optional<Member> kakaoIncludedMember = memberRepository.findByKakaoEmail(email);
+
+        if (kakaoIncludedMember.isEmpty()) {
+            member.updateKakaoEmail(email);
+            member.updateKakaoRefreshToken(userInfo.kakaoRefreshToken());
+        }
+
+        return MemberResponseDto.KakaoTokenInfo.builder()
+                .kakaoAccessToken(userInfo.kakaoAccessToken())
                 .build();
     }
 }
