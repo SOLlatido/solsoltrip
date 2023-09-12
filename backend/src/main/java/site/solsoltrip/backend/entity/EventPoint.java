86a0d5aef36f79a91478409ce03d80cfcfcb8231
leverest96 <myjade0416@gmail.com>
2023-09-12 14:23:28 +0900

    feat: 이벤트 관련 포인트 엔티티 추가

---
+++ backend/src/main/java/site/solsoltrip/backend/entity/EventPoint.java
@@ -0,0 +1,28 @@
+package site.solsoltrip.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "event_point")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@AllArgsConstructor
+@Builder
+public class EventPoint {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long eventPointSeq;
+
+    @JoinColumn(name = "member_seq")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    private String name;
+
+    private Integer point;
+
+    private LocalDateTime acceptedDate;
+}
