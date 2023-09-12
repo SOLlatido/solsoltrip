05c57f098b768cfac00e7fbacf7b200a5b8dac35
leverest96 <myjade0416@gmail.com>
2023-09-12 12:49:18 +0900

    refactor: 멤버 엔티티 수정으로 인한 멤버 controller 수정

--- backend/src/main/java/site/solsoltrip/backend/controller/MemberController.java
+++ backend/src/main/java/site/solsoltrip/backend/controller/MemberController.java
@@ -1,29 +1,19 @@
 package site.solsoltrip.backend.controller;
 
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import site.solsoltrip.backend.dto.KakaoUserInfo;
 import site.solsoltrip.backend.dto.MemberRequestDto;
 import site.solsoltrip.backend.dto.MemberResponseDto;
 import site.solsoltrip.backend.service.MemberService;
-import site.solsoltrip.backend.utility.CookieUtility;
 
 @RestController
 @RequestMapping("/api/member")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
-
-    @PostMapping("/signup")
-    public ResponseEntity<Void> signup(@RequestBody @Validated final MemberRequestDto.signup requestDto) {
-        memberService.signup(requestDto);
-
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
 
     @GetMapping("/login")
     public ResponseEntity<MemberResponseDto.login> login(@RequestBody @Validated final MemberRequestDto.login requestDto) {
@@ -32,8 +22,8 @@ public class MemberController {
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
-    @GetMapping("/kakao")
-    public ResponseEntity<MemberResponseDto.KakaoTokenInfo> kakaoSync(@RequestParam final String code, @RequestParam final Long state, HttpServletResponse response) {
+    @PostMapping("/kakao")
+    public ResponseEntity<MemberResponseDto.KakaoTokenInfo> kakaoSync(@RequestParam final String code, @RequestParam final Long state) {
         MemberResponseDto.KakaoTokenInfo responseDto = memberService.kakaoSync(code, state);
 
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
