bb6fb010bae8dee3b66500597671da4762bd678d
leverest96 <myjade0416@gmail.com>
2023-09-12 12:49:03 +0900

    refactor: 멤버 엔티티 수정으로 인한 멤버 service 수정

--- backend/src/main/java/site/solsoltrip/backend/service/MemberService.java
+++ backend/src/main/java/site/solsoltrip/backend/service/MemberService.java
@@ -1,13 +1,11 @@
 package site.solsoltrip.backend.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import site.solsoltrip.backend.dto.MemberRequestDto;
 import site.solsoltrip.backend.dto.MemberResponseDto;
 import site.solsoltrip.backend.entity.Member;
-import site.solsoltrip.backend.entity.Role;
 import site.solsoltrip.backend.oauth.KakaoOAuth2;
 import site.solsoltrip.backend.repository.MemberRepository;
 
@@ -21,38 +19,10 @@ public class MemberService {
 
     private final KakaoOAuth2 kakaoOAuth2;
 
-    private final PasswordEncoder passwordEncoder;
-
-    public boolean checkEmailExistence(final String id) {
-        return memberRepository.findById(id).isPresent();
-    }
-
-    @Transactional
-    public void signup(final MemberRequestDto.signup requestDto) {
-        if (checkEmailExistence(requestDto.id())) {
-            throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
-        }
-
-        final Member member = Member.builder()
-                .id(requestDto.id())
-                .password(passwordEncoder.encode(requestDto.password()))
-                .name(requestDto.name())
-                .point(0)
-                .phone(requestDto.phone())
-                .role(Role.ADMIN)
-                .build();
-
-        memberRepository.save(member);
-    }
-
     public MemberResponseDto.login login(final MemberRequestDto.login requestDto) {
-        final Member member = memberRepository.findById(requestDto.id()).orElseThrow(
+        final Member member = memberRepository.findByUuid(requestDto.uuid()).orElseThrow(
                 () -> new IllegalArgumentException("존재하지 않는 유저입니다.")
         );
-
-        if (!passwordEncoder.matches(requestDto.password(), member.getPassword())) {
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
-        }
 
         String accessToken = "access-token";
 
@@ -68,11 +38,17 @@ public class MemberService {
 
         return kakaoTokenInfo == null ?
                 MemberResponseDto.login.builder()
+                        .memberSeq(member.getMemberSeq())
+                        .uuid(member.getUuid())
                         .name(member.getName())
+                        .point(member.getPoint())
                         .accessToken(accessToken)
                         .build() :
                 MemberResponseDto.login.builder()
+                        .memberSeq(member.getMemberSeq())
+                        .uuid(member.getUuid())
                         .name(member.getName())
+                        .point(member.getPoint())
                         .accessToken(accessToken)
                         .kakaoAccessToken(kakaoTokenInfo.kakaoAccessToken())
                         .build();
