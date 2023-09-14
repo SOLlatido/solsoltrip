b7ab664b8aa702136e6fde61983a8a97ced151a4
leverest96 <myjade0416@gmail.com>
2023-09-15 01:41:24 +0900

    feat: AWS S3 설정을 위한 config 추가 및 manager 파일 생성

--- backend/src/main/java/site/solsoltrip/backend/config/AppConfig.java
+++ backend/src/main/java/site/solsoltrip/backend/config/AppConfig.java
@@ -1,14 +1,30 @@
 package site.solsoltrip.backend.config;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import site.solsoltrip.backend.properties.aws.AwsProperties;
+import site.solsoltrip.backend.properties.aws.AwsS3Properties;
+import site.solsoltrip.backend.util.AwsS3Manager;
 
 @Configuration
 public class AppConfig {
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public AmazonS3 amazonS3(final AwsProperties awsProperties, final AwsS3Properties awsS3Properties) {
+        final AWSCredentials credentials = new BasicAWSCredentials(awsProperties.getAccessKey(), awsProperties.getSecretKey());
+
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(awsS3Properties.getRegion())
+                .build();
+    }
+
+    @Bean
+    public AwsS3Manager awsS3Manager(final AmazonS3 amazonS3, final AwsS3Properties awsS3Properties) {
+        return new AwsS3Manager(amazonS3, awsS3Properties.getBucket());
     }
 }
