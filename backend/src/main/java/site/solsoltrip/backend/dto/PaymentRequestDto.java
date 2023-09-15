b635dc1c41e7fb2839151a05d91753d25d7471f4
leverest96 <leverest96@gmail.com>
2023-09-16 01:48:29 +0900

    feat: 출금 및 인원별 출금액 수정 기능을 위한 request dto 추가

--- backend/src/main/java/site/solsoltrip/backend/dto/PaymentRequestDto.java
+++ backend/src/main/java/site/solsoltrip/backend/dto/PaymentRequestDto.java
@@ -1,7 +1,14 @@
 package site.solsoltrip.backend.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public class PaymentRequestDto {
     public record paymentDetail(
@@ -18,5 +25,43 @@ public class PaymentRequestDto {
 
             String memo,
 
-            MultipartFile pictureFile) {}
+            MultipartFile pictureFile,
+
+            List<EachExpense> eachExpenseList) {}
+
+    public record withdraw(
+            @NotNull
+            Long accompanySeq,
+
+            @NotBlank
+            String store,
+
+            @NotNull
+            Integer cost,
+
+            @NotNull
+            LocalDate acceptedDate,
+
+            @NotBlank
+            String category,
+
+            @NotNull
+            LocalDateTime acceptedDateTime,
+
+            @NotBlank
+            String memo,
+
+            @NotBlank
+            String picture,
+
+            @NotNull
+            LocalDateTime memoDateTime
+    ) {}
+
+    @Getter
+    @Builder
+    public static class EachExpense {
+        private final Long memberSeq;
+        private final Double cost;
+    }
 }
