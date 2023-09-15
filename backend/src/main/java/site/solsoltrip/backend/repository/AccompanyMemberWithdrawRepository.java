e25160fc3a541578081332db3ede22895c177789
leverest96 <leverest96@gmail.com>
2023-09-16 01:51:26 +0900

    feat: 동행 통장 출금 내역 repository 수정
    출금 후 바로 해당 내역 seq 번호를 확인하기 기능 추가

--- backend/src/main/java/site/solsoltrip/backend/repository/AccompanyMemberWithdrawRepository.java
+++ backend/src/main/java/site/solsoltrip/backend/repository/AccompanyMemberWithdrawRepository.java
@@ -15,15 +15,12 @@ public interface AccompanyMemberWithdrawRepository extends JpaRepository<Accompa
     @Query("select a from AccompanyMemberWithdraw a join fetch a.accompany where a.accompany.accompanySeq = :accompanySeq")
     List<AccompanyMemberWithdraw> findByAccompanySeq(@Param("accompanySeq") final Long accompanySeq);
 
+    @Query("select a from AccompanyMemberWithdraw a join fetch a.accompany where a.accompany.accompanySeq = :accompanySeq order by a.accompanyMemberWithdrawSeq desc")
+    List<AccompanyMemberWithdraw> findByAccompanySeqOrderByAccompanyMemberWithdrawSeqDesc(@Param("accompanySeq") final Long accompanySeq);
+
     @Query("select a from AccompanyMemberWithdraw a join fetch a.accompany where a.accompany.accompanySeq = :accompanySeq and a.category = :category")
     List<AccompanyMemberWithdraw> findByAccompanySeqAndCategory(@Param("accompanySeq") final Long accompanySeq, @Param("category")  final int category);
 
     @Query("select a from AccompanyMemberWithdraw a join fetch a.accompany where a.accompany.accompanySeq = :accompanySeq and a.acceptedDate = :acceptedDate")
     List<AccompanyMemberWithdraw> findByAccompanySeqAndAcceptedDate(@Param("accompanySeq") final Long accompanySeq, @Param("acceptedDate") final LocalDate acceptedDate);
-
-    @Query(value = "select a from AccompanyMemberWithdraw a " +
-            "join fetch a.accompany join fetch a.member " +
-            "where a.accompany.accompanySeq = :accompanySeq and a.member.memberSeq = :memberSeq",
-            nativeQuery = true)
-    List<AccompanyMemberWithdraw> findByAccompanySeqAndMemberSeq(@Param("accompanySeq") final Long accompanySeq, @Param("memberSeq") final Long memberSeq);
 }
