5c1646c5e3c0b295672a815331a909c3d67ed103
leverest96 <leverest96@gmail.com>
2023-09-16 15:37:49 +0900

    fix: 정산 controller에 requestbody와 유효성 검사 추가

--- backend/src/main/java/site/solsoltrip/backend/controller/SettlementController.java
+++ backend/src/main/java/site/solsoltrip/backend/controller/SettlementController.java
@@ -1,9 +1,9 @@
 package site.solsoltrip.backend.controller;
 
-import com.amazonaws.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import site.solsoltrip.backend.dto.SettlementRequestDto;
 import site.solsoltrip.backend.dto.SettlementResponseDto;
@@ -16,35 +16,35 @@ public class SettlementController {
     private final SettlementService settlementService;
 
     @PatchMapping("/reset")
-    public ResponseEntity<Void> resetEndTime(final SettlementRequestDto.resetEndTime requestDto) {
+    public ResponseEntity<Void> resetEndTime(@RequestBody @Validated final SettlementRequestDto.resetEndTime requestDto) {
         settlementService.resetEndTime(requestDto);
 
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @PatchMapping("/manual")
-    public ResponseEntity<Void> endTrip(final SettlementRequestDto.endTrip requestDto) {
+    public ResponseEntity<Void> endTrip(@RequestBody @Validated final SettlementRequestDto.endTrip requestDto) {
         settlementService.endTrip(requestDto);
 
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @PostMapping("/result")
-    public ResponseEntity<SettlementResponseDto.showTripResult> showTripResult(final SettlementRequestDto.showTripResult requestDto) {
+    public ResponseEntity<SettlementResponseDto.showTripResult> showTripResult(@RequestBody @Validated final SettlementRequestDto.showTripResult requestDto) {
         SettlementResponseDto.showTripResult responseDto = settlementService.checkShowTripResult(requestDto);
 
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
     @PostMapping("/timeline")
-    public ResponseEntity<SettlementResponseDto.timeline> timeline(final SettlementRequestDto.timeline requestDto) {
+    public ResponseEntity<SettlementResponseDto.timeline> timeline(@RequestBody @Validated final SettlementRequestDto.timeline requestDto) {
         final SettlementResponseDto.timeline responseDto = settlementService.timeline(requestDto);
 
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 
     @PatchMapping("/settle")
-    public ResponseEntity<SettlementResponseDto.settleUp> settleUp(final SettlementRequestDto.settleUp requestDto) {
+    public ResponseEntity<SettlementResponseDto.settleUp> settleUp(@RequestBody @Validated final SettlementRequestDto.settleUp requestDto) {
         final SettlementResponseDto.settleUp responseDto = settlementService.settleUp(requestDto);
 
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
