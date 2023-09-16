f07a3bc35f769d1bd71ae5f6b8c10fc70ce72d83
leverest96 <leverest96@gmail.com>
2023-09-16 15:40:41 +0900

    feat: 신한 api 사용에 쓰이는 dto에 실명 확인 로직 추가

--- backend/src/main/java/site/solsoltrip/backend/dto/ShbhackRequestDto.java
+++ backend/src/main/java/site/solsoltrip/backend/dto/ShbhackRequestDto.java
@@ -20,4 +20,43 @@ public class ShbhackRequestDto {
     public static class DataBody {
         private String 실명번호;
     }
+
+    @Getter
+    @Builder
+    public static class AccountList {
+        private ShbhackRequestDto.AccountList.DataHeader dataHeader;
+
+        private ShbhackRequestDto.AccountList.DataBody dataBody;
+
+        @Getter
+        public static class DataHeader {
+            private final String apikey = "2023_Shinhan_SSAFY_Hackathon";
+        }
+
+        @Builder
+        @Getter
+        public static class DataBody {
+            private String 실명번호;
+        }
+    }
+
+    @Getter
+    @Builder
+    public static class CheckName {
+        private ShbhackRequestDto.CheckName.DataHeader dataHeader;
+
+        private ShbhackRequestDto.CheckName.DataBody dataBody;
+
+        @Getter
+        public static class DataHeader {
+            private final String apikey = "2023_Shinhan_SSAFY_Hackathon";
+        }
+
+        @Builder
+        @Getter
+        public static class DataBody {
+            private String 입금은행코드;
+            private String 입금계좌번호;
+        }
+    }
 }
